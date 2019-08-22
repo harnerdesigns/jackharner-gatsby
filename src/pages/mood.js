7fd3400e40d2bddfceec48639f8e6248ccea5316
Jack Harner <social@shoolu.com>
2019-08-22 16:46:25 -0600

    adding Page Title to mood page

--- src/pages/mood.js
+++ src/pages/mood.js
@@ -4,10 +4,12 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import GoesRight from "../components/mood/goesRight"
+import PageTitle from "../components/pageTitle";
 
 const Mood = () => (
   <Layout>
     <SEO title="Mood" />
+    <PageTitle>Mood</PageTitle>
     <GoesRight />
   </Layout>
 )
