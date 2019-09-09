5f90c03dd73757d6cb94e3863520d2696141127f
Jack Harner <social@shoolu.com>
2019-09-09 10:25:58 -0600

    darker color text value

--- src/components/common/colorSwitcher.js
+++ src/components/common/colorSwitcher.js
@@ -50,8 +50,8 @@ export default class ColorSwitcher extends Component {
 
 
 
-    if (cookies.get('paletteColor')) {
-      const cookieColor = cookies.get('paletteColor');
+    if (cookies.get('paletteColorObj')) {
+      const cookieColor = cookies.get('paletteColorObj');
       this.state = {
         color: cookieColor,
         selected_color: cookieColor,
@@ -98,17 +98,49 @@ export default class ColorSwitcher extends Component {
   }
 
 
+  hslToRgb = (h, s, l) => {
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+
+  return {r: r * 255, g: g * 255, b: b * 255};
+}
+
+
   getTextColor = (c) => {
 
     let o = Math.round(((parseInt(c.r) * 299) + (parseInt(c.g) * 587) + (parseInt(c.b) * 114)) / 1000);
     return ((o > 125) ? ('black') : ('white'));
   }
 
-  darkenHSL(hsl, amount) {
-    console.log({ hsl: hsl });
+  darkenHSL(hsl, amount, format=false) {
 
-    let newL = (hsl.l * 100) - amount;
-    return "hsl(" + hsl.h + ", " + (hsl.s * 100) + "%, " + newL + "%)"
+    let newL = (hsl.l) - (amount / 100);
+    if (format) {
+
+      return "hsl(" + hsl.h + ", " + (hsl.s * 100) + "%, " + (newL * 100)  + "%)"
+    } else {
+
+      return {h:hsl.h, s: hsl.s, l: newL}
+    }
   }
 
 
@@ -120,7 +152,10 @@ export default class ColorSwitcher extends Component {
     document.documentElement.style.setProperty('--text-color', this.getTextColor(this.state.selected_color.rgb));
 
 
-    document.documentElement.style.setProperty('--darkerColor', this.darkenHSL(this.state.selected_color.hsl, 2));
+    document.documentElement.style.setProperty('--darkerColor', this.darkenHSL(this.state.selected_color.hsl, 15, true));
+    let darkerColor = this.darkenHSL(this.state.selected_color.hsl, 15);
+    console.log({lighter: this.state.selected_color.hsl, darker: darkerColor});
+    document.documentElement.style.setProperty('--darker-text-color', this.getTextColor(this.hslToRgb(darkerColor.h, darkerColor.s, darkerColor.l)));
 
 
 
@@ -145,15 +180,13 @@ export default class ColorSwitcher extends Component {
 
   onAcceptColor = () => {
 
-
-
     // update the state with the new array of colors
     this.setState({ color: this.state.selected_color, isColorPickerOpen: !this.state.isColorPickerOpen })
 
     this.setColorVars();
 
 
-    cookies.set('paletteColor', this.state.selected_color, { path: '/' });
+    cookies.set('paletteColorObj', this.state.selected_color, { path: '/' });
   }
 
 
