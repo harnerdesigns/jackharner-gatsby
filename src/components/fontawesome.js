c77e6e518fa8a36e690ac99426abd64793d9368a
Jack Harner <jack@harnerdesigns.com>
2019-09-14 21:57:10 -0600

    Added Envelope

--- src/components/fontawesome.js
+++ src/components/fontawesome.js
@@ -1,7 +1,7 @@
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee, faCaretDown, faPalette } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faEnvelope, faCoffee, faCaretDown, faPalette } from '@fortawesome/free-solid-svg-icons'
 
 
-library.add(fab, faCheckSquare, faCoffee, faCaretDown, faPalette)
+library.add(fab, faCheckSquare, faEnvelope, faCoffee, faCaretDown, faPalette)
