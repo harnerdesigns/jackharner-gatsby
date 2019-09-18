58cb6c7c87557b684d4b21a33ef422040cade109
Jack Harner <jack@harnerdesigns.com>
2019-09-17 21:02:27 -0600

    Contact Page Mobile Styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,7 +18,7 @@ const Contact = () => (
 <a href="mailto:jack@jackharner.com" className="button button--large"><FontAwesomeIcon icon="envelope"/> Email</a>      
       </section>
       <h3>Please Reach Out If You Are:</h3>
-      <ul style={{fontSize: "1.5em", width: "50%"}}>
+      <ul>
         <li>Looking for a Freelance Web Developer (<a href="https://harnerdesigns.com">Here's My Portfolio</a>)</li>
         <li>Hiring for a Front-End Developer Position (<a href="https://resume.jackharner.com">Here's My Resume</a>)</li>
         <li>Literally drowning in so much money that you need to give it away to some stranger online</li>
