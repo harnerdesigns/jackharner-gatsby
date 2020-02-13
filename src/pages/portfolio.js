aa924b35bba1b498f66bf427605941d5a17297f8
Jack Harner <social@shoolu.com>
2020-02-13 15:03:20 -0700

    tags

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -63,35 +63,40 @@ const Blog = ({ data }) => {
 export default Blog
 
 export const pageQuery = graphql`
-  query AllBlogsQuery {
-    allMarkdownRemark(
-      sort: {
-        order: [DESC, DESC]
-        fields: [fields___weight, frontmatter___date]
-      }
-      filter: { fields: { published: { eq: true } } }
-    ) {
-      edges {
-        node {
-          excerpt(pruneLength: 250)
-          id
-          frontmatter {
-            title
-            description
-            color
-            date(formatString: "MMMM DD, YYYY")
-            tags
-            logo {
-              extension
-              publicURL
-            }
-          }
-          fields {
-            slug
-            collection
-          }
-        }
-      }
-    }
-  }
-`
+         query AllBlogsQuery {
+           allMarkdownRemark(
+             sort: {
+               order: [DESC, DESC]
+               fields: [fields___weight, frontmatter___date]
+             }
+             filter: {
+               fields: {
+                 published: { eq: true }
+                 collection: { eq: "portfolio"   }
+               }
+             }
+           ) {
+             edges {
+               node {
+                 excerpt(pruneLength: 250)
+                 id
+                 frontmatter {
+                   title
+                   description
+                   color
+                   date(formatString: "MMMM DD, YYYY")
+                   tags
+                   logo {
+                     extension
+                     publicURL
+                   }
+                 }
+                 fields {
+                   slug
+                   collection
+                 }
+               }
+             }
+           }
+         }
+       `
--- src/templates/tag.js
+++ src/templates/tag.js
@@ -3,7 +3,6 @@ import Helmet from "react-helmet"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import BlogCard from "../components/blog/blogCard"
-import RssCard from "../components/blog/rssCard"
 import PageTitle from "../components/pageTitle"
 import ProjectCard from "../components/portfolio/projectCard"
 
@@ -12,13 +11,6 @@ const _ = require("lodash")
 class TagRoute extends React.Component {
   render() {
     const posts = this.props.data.allMarkdownRemark.edges
-    const postLinks = posts.map(post => (
-      <li key={post.node.fields.slug}>
-        <Link to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
-        </Link>
-      </li>
-    ))
     const tag = this.props.pageContext.tag
     const title = this.props.data.site.siteMetadata.title
     const totalCount = this.props.data.allMarkdownRemark.totalCount
