834f0c48563b3ef361811bf4bf4cd6b4ecacb4c9
Jack Harner <jack@harnerdesigns.com>
2020-08-09 11:52:18 -0600

    clean up imports on quotes

--- src/components/testimonies/quote.js
+++ src/components/testimonies/quote.js
@@ -1,13 +1,9 @@
-import React, { Component } from "react"
-
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React from "react"
 
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-import defaultOGImage from '../../images/jackHarner-default-OG.jpg'
+
+
 
 const QuoteCard = ({ quote }) => {
 
--- src/components/testimonies/quotes.js
+++ src/components/testimonies/quotes.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -7,7 +7,6 @@ import "slick-carousel/slick/slick-theme.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-import defaultOGImage from '../../images/jackHarner-default-OG.jpg'
 import QuoteCard from "./quote";
 
 const Quotes = ({ quotes }) => {
