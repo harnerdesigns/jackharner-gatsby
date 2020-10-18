0c3c7fc0748d8729100a99fb600cb7a3eea0ae93
Jack Harner <jack@harnerdesigns.com>
2020-10-18 11:31:52 -0600

    ConverKit Blog Post

--- src/templates/newsletter.js
+++ src/templates/newsletter.js
@@ -4,54 +4,76 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { NewsletterForm } from "../components/common/NewsletterForm"
-import  ogImage from "../images/Learn-With-Me-OG-Image.jpg"
+import ogImage from "../images/Learn-With-Me-OG-Image.jpg"
 import Button from "../components/atoms/button"
 
-
-
-
-
-const About = ({ data, pageContext }) => {
-  const { recentEmails, subscriberCount } = pageContext
-  console.log(recentEmails)
-  const ref = React.createRef();
+const Newsletter = ({
+  data,
+  pageContext: { recentEmails, subscriberCount },
+}) => {
+  const ref = React.createRef()
 
   return (
     <Layout footerCTA={false}>
-      <SEO title="Learn _____ With Me" image={ogImage} description="Sign Up for my newsletter and learn about Web Development, Graphic Design, Automation, & More!" />
+      <SEO
+        title="Learn _____ With Me"
+        image={ogImage}
+        description="Sign Up for my newsletter and learn about Web Development, Graphic Design, Automation, & More!"
+      />
       <container className="half white">
-
         <NewsletterForm ref={ref} />
       </container>
 
       <container className="slim pink">
-        <h2>I only send <span title="For Legal Reasons, This Is A JOKE.">EXPERTLY CRAFTED EMAILS</span> like:</h2> 
+        <h2>
+          I only send{" "}
+          <span title="For Legal Reasons, This Is A JOKE.">
+            EXPERTLY CRAFTED EMAILS
+          </span>{" "}
+          like:
+        </h2>
 
         <div className="recentEmails">
           {recentEmails.map(({ node: email }) => {
             return (
               <Link to={email.fields.slug} className="newsletter__card">
                 <h3>{email.frontmatter.title}</h3>
-                {email.frontmatter.subtitle && <h4>{email.frontmatter.subtitle}</h4>}
+                {email.frontmatter.subtitle && (
+                  <h4>{email.frontmatter.subtitle}</h4>
+                )}
               </Link>
             )
           })}
         </div>
-        <h2>Get them before anyone else by <a style={{cursor: 'pointer'}} onClick={() => {ref.current.focus()}} >Signing Up</a>!</h2>
-
+        <h2>
+          Get them before anyone else by{" "}
+          <a
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              ref.current.focus()
+            }}
+          >
+            Signing Up
+          </a>
+          !
+        </h2>
       </container>
-      <container className="half black">
-
-
-          <h1>Join {subscriberCount} Other Subscribers Today!</h1>
-          <Button onClick={() => {ref.current.focus()}} size="large" extraStyle={{width: "50%"}} label="Let's Go!!!"/>
-
-      </container>
+      <section className="half black">
+        <h1>Join {subscriberCount} Other Subscribers Today!</h1>
+        <Button
+          onClick={() => {
+            ref.current.focus()
+          }}
+          size="large"
+          extraStyle={{ width: "50%" }}
+          label="Let's Go!!!"
+        />
+      </section>
     </Layout>
   )
 }
 
-export default About
+export default Newsletter
 
 export const pageQuery = graphql`
   query MailingListQuery {
