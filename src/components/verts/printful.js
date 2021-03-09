27391a13bc6ebdc33a7e4fc606fbb38eb2a5db14
Jack Harner <jack@harnerdesigns.com>
2021-03-08 22:54:53 -0700

    Printful Vert

---
+++ src/components/verts/printful.js
@@ -0,0 +1,16 @@
+import React from "react"
+import printfulIcon from "../../images/printfulLogo.png"
+import Vert from "./vert"
+
+const Printful = () => {
+  const link = "https://www.printful.com/a/643178:255a83d8b94d873cd78eec57538ba387"
+  return (
+    <Vert link={link} image={ printfulIcon } slug="printful" buttonText="Start Selling Merch Today &raquo;" >      
+    <h2>Got Designs? Sell Them With ZERO Hassle.</h2>
+    <h3>Printful Prints, Packs, & Ships On 100s of Products</h3>
+    </Vert>
+
+  )
+}
+
+export default Printful
