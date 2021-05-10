86c600027d49b00c71f13df1b93d3782c4896968
Jack Harner <jack@harnerdesigns.com>
2021-05-09 18:42:59 -0600

    Full Time Freelancer Counter

--- src/components/freelanceCountdown.js
+++ src/components/freelanceCountdown.js
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Link } from "gatsby"
 import React, { useEffect, useRef, useState } from "react"
 
@@ -8,17 +9,16 @@ const FreelanceCountdown = () => {
   const [timeLeft, setTimeLeft] = useState({})
   const [year, setYear] = useState(new Date().getFullYear())
   const calculateTimeLeft = () => {
-    const difference = +new Date(`2021-04-30T16:00:00.000-06:00`) - +new Date()
+    const difference =  +new Date() - +new Date(`2021-04-30T16:00:00.000-06:00`)
 
-    let timeLeft = {}
-    if (difference > 0) {
-      timeLeft = {
+    let timeLeft = 
+       {
         days: Math.floor(difference / (1000 * 60 * 60 * 24)),
         hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
         minutes: Math.floor((difference / 1000 / 60) % 60),
         seconds: Math.floor((difference / 1000) % 60),
       }
-    }
+    
     return timeLeft
   }
   useEffect(() => {
@@ -39,7 +39,7 @@ const FreelanceCountdown = () => {
     let string = timeLeft[interval]
     timerComponents.push(
       <>
-        <CountdownNumber>{string.toString().padStart(2, "0")}</CountdownNumber>
+        <CountdownNumber>{string.toString().padStart(2, "0")} <CountdownLabel>{interval}</CountdownLabel></CountdownNumber>
         <CountdownSeparator>{i == 3 ? "" : ":"}</CountdownSeparator>
       </>
     )
@@ -47,9 +47,9 @@ const FreelanceCountdown = () => {
 
   return (
     <section className="half black">
-      <h1 style={{textAlign: "center"}}>Countdown Till I Go Fulltime Freelance</h1>
+      <h1 style={{textAlign: "center"}}>I've Been A Fulltime Freelancer For</h1>
       <CountdownWrapper>
-        {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+        {timerComponents.length ? timerComponents : <FontAwesomeIcon icon="spinner" spin />}
       </CountdownWrapper>
       <h3 style={{textAlign: "center"}}>Hiring a contract Web Developer? <Link to="/contact">Let's Chat &raquo;</Link></h3>
     </section>
@@ -59,9 +59,11 @@ const FreelanceCountdown = () => {
 export default FreelanceCountdown
 
 const CountdownWrapper = styled.div`
-  display: flex;
+  display: grid;
   align-items: center;
   justify-content: center;
+  grid-template-columns: repeat(3, 1fr auto) 1fr;
+  grid-gap: 1ch;
 
   h1 {
     text-align: center;
@@ -72,20 +74,30 @@ const CountdownNumber = styled.span`
   font-size: 3rem;
   font-weight: 900;
   /* vertical-align: middle; */
-  margin: 0.5ch;
+  width: 100%;
   line-height: 1;
   background: var(--color);
   color: var(--text-color);
   border-radius: var(--border-radius);
   padding: 0.5ch;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 
   @media ${breakpoints.laptop} {
     font-size: 4rem;
   }
 `
 
+const CountdownLabel = styled.span`
+font-size: 1.25rem;
+font-weight: 400;
+`
+
 const CountdownSeparator = styled.span`
   font-size: 2.5rem;
   /* font-weight: 100; */
   line-height: 1;
+  display: block;
 `
