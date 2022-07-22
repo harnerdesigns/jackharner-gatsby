3ce25516431ad7f2091450b6fcb979d3c28b1f59
Jack Harner <jack@harnerdesigns.com>
2022-07-22 00:41:55 -0600

    Quick Links

---
+++ src/components/common/footer-min.js
@@ -0,0 +1,52 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import PropTypes from "prop-types"
+import React from "react"
+import styled from "styled-components"
+import { breakpoints } from "../breakpoints"
+
+import FooterCTA from "./FooterCTA"
+
+import MainMenu from "./mainMenu"
+import SecondaryMenu from "./secondaryMenu"
+
+const Footer = ({ siteTitle, footerCTA = true }) => (
+  <footer className="slim main_footer">
+    {footerCTA && <FooterCTA />}
+
+    <MenuGrid>
+      <section className="copyright">
+        &copy; 2020 - 2022 Jack Harner |{" "}
+        <a href="https://github.com/harnerdesigns/jackharner-gatsby">
+          {" "}
+          Built With Gatsby
+        </a>
+      </section>
+    </MenuGrid>
+  </footer>
+)
+
+Footer.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Footer.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Footer
+
+const MenuGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  width: 100%;
+
+  @media ${breakpoints.laptopL} {
+    width: 50%;
+  }
+
+  .main_nav {
+    a {
+      width: 100%;
+    }
+  }
+`
---
+++ src/components/layout-min.js
@@ -0,0 +1,45 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { MDXProvider } from "@mdx-js/react"
+import { useStaticQuery, graphql } from "gatsby"
+import "./fontawesome"
+import Header from "./common/header-min"
+import "../scss/style.scss"
+import Footer from "./common/footer-min"
+import SimpleReactLightbox from "simple-react-lightbox";
+import ImageGrid from "./imageGrid";
+
+
+const Layout = ({ children, footerCTA }) => {
+
+  const shortcodes = { ImageGrid }
+  return (
+    <MDXProvider components={shortcodes}>
+
+      <SimpleReactLightbox>
+
+    
+      <div>
+        <Header/>
+
+        {children}
+
+        <Footer footerCTA={false} />
+      </div>
+    </SimpleReactLightbox>
+    </MDXProvider>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
