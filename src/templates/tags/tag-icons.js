87f0def94cfbbfd2aabc06abcda0b89180ede65d
Jack Harner <jack@jackharner.com>
2025-05-17 09:12:26 -0600

    Clean Up

--- src/templates/tags/tag-icons.js
+++ src/templates/tags/tag-icons.js
@@ -35,7 +35,6 @@ const tagIcons = {
   "Code Snippets": 'code',
   Writing: "pencil-alt",
   "Non Profit": "tree",
-  Cannabis: "cannabis",
   Career: "user-astronaut",
   Freelance: "bacon",
   DigitalOcean: ["fab", "digital-ocean"],
